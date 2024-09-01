9344cd92deebaaadc8a1dcedd2aab0930bc82cb0
xSeri98 <ronquillo.236225@pasay-edsa.sti.edu.ph>
2024-09-01 17:42:34 +0800

    Added chat list and add icon on homepage

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonPopover, IonLabel } from '@ionic/angular/standalone';
-import { ellipsisVerticalOutline } from 'ionicons/icons';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonPopover, IonLabel, IonSegment, SegmentChangeEventDetail, IonSegmentButton, IonList, IonListHeader, IonItem, IonAvatar, IonText, IonFab, IonFabButton } from '@ionic/angular/standalone';
+import { ellipsisVerticalOutline,add } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { IonSegmentCustomEvent } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonPopover, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonFabButton, IonFab, IonText, IonAvatar, IonItem, IonListHeader, IonList, IonSegmentButton, IonSegment, IonLabel, IonPopover, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
+newChat() {
+throw new Error('Method not implemented.');
+}
+segment = 'chats';
+onSegmentChanged($event: IonSegmentCustomEvent<SegmentChangeEventDetail>) {
+throw new Error('Method not implemented.');
+}
 logout() {
 throw new Error('Method not implemented.');
 }
 
   constructor() 
   {
-    addIcons({ ellipsisVerticalOutline });
+    addIcons({ ellipsisVerticalOutline,add });
 
    }
 
