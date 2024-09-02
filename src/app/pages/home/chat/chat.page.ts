fb2fffa9e042f2bb0b2124839db97f748c2f7153
xSeri98 <ronquillo.236225@pasay-edsa.sti.edu.ph>
2024-09-02 17:31:23 +0800

    Partially completed textbox on chat page.

--- src/app/pages/home/chat/chat.page.ts
+++ src/app/pages/home/chat/chat.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonBackButton,IonButtons,IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonTextarea,IonFooter,IonBackButton,IonButtons,IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './chat.page.html',
   styleUrls: ['./chat.page.scss'],
   standalone: true,
-  imports: [IonBackButton,IonButtons,IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonTextarea,IonFooter,IonBackButton,IonButtons,IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ChatPage implements OnInit {
   id: string | null = null;
